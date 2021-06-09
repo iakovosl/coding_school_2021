16a64ee54ad2e1cb3c5990080458d265e527c17e
iakovosl <iakovoslampadakis@gmail.com>
2021-06-09 15:13:11 +0300

    add session_2 solutions

---
+++ Session_2/ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Sum(int num1, int num2) {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+    }
+}
---
+++ Session_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+
+            //question1
+            //Console.WriteLine("Hello Iakovos!");
+
+            //Console.ReadLine();
+
+            //question2
+            //int x = 5;
+            //int y = 6;
+            //int sum = x + y;
+            //double divide = x / (double)y;
+            //Console.WriteLine(sum); // Print the sum of x + y
+            //Console.WriteLine(divide);
+            //Console.ReadLine();
+
+            //question3
+            // System.Console.WriteLine(-1 + 5 * 6);
+
+            // System.Console.WriteLine((38 + 5) % 7);
+
+            // System.Console.WriteLine(14 + (-3 * 6 / 7));
+
+            // System.Console.WriteLine(2 + 13 / 6 * 6 + Math.Sqrt(7));
+
+            // System.Console.WriteLine((Math.Pow(6, 4) + Math.Pow(5, 7))/ (9 % 4));
+
+            // Console.ReadLine();
+
+
+            //question4
+            //int num1, num2, num3;
+
+            //Console.Write("Input the first number to multiply: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the second number to multiply: ");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the third number to multiply: ");
+            //num3 = Convert.ToInt32(Console.ReadLine());
+
+            //int result = num1 * num2 * num3;
+            //Console.WriteLine("Output: {0} x {1} x {2} = {3}",
+            //num1, num2, num3, result);
+            //Console.ReadLine();
+
+            //question5
+            //int num1, num2, num3, num4, num5;
+
+            //Console.Write("Input the first number to avarage: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the second number to avarage: ");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the third number to avarage: ");
+            //num3 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the third number to avarage: ");
+            //num4 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input the third number to average: ");
+            //num5 = Convert.ToInt32(Console.ReadLine());
+
+            //int result = (num1 + num2 + num3 +num4 +num5)/5;
+            //Console.WriteLine("The average of {0}, {1}, {2}, {3}, {4} is: {5} ",
+            //num1, num2, num3, num4,num5, result);
+            //Console.ReadLine();
+
+            //question6
+            // int age;
+            // string gender;
+            // Console.Write("Enter your age ");
+            // age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter your gender ");
+            // gender = Convert.ToString(Console.ReadLine());
+            // Console.Write("You look younger than {0} and your gender is {1} ", age, gender);
+            // Console.ReadLine();
+
+            //question7
+            //int n = 15665;
+            //ConvertSeconds(n);
+            //Console.ReadLine();
+
+            //question9
+            //Console.Write("Enter the amount of Celsius: ");
+            //int celsius = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Kelvin = {0}", celsius + 273);
+            //Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+            //Console.ReadLine();
+
+
+            //question10
+            //Console.Write("Please Enter a phrase: ");
+            //string phrase = (Console.ReadLine());
+            //phrase = phrase.Replace("a", "");
+            //Console.WriteLine(phrase);
+            //Console.ReadLine();
+
+
+        }
+
+        //static void ConvertSeconds(int n) {
+            //int minutes, hours;
+
+            //minutes = n / 60;
+            //hours = n / 3600;
+            //double days = n * 1.15 * Math.Pow(10, -5);
+            //double years = n * 3.16 * Math.Pow(10, -8);
+
+            //Console.WriteLine("Minutes = " + minutes +", Hours = " + hours + "Days = " +days+ " Years: "+years);
+        //}
+
+
+    }
+}
