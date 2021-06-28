c9f38e4241a4364180273b5f4084b7a370d97873
iakovosl <iakovoslampadakis@gmail.com>
2021-06-29 01:34:01 +0300

    Added student and course classes

---
+++ Session_18/UnivesrityApp/UniversityApp.Model/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityApp.Model {
+    class Course {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ Session_18/UnivesrityApp/UniversityApp.Model/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityApp.Model {
+    class Students {
+        public string Name { get; set; }
+        public string Sutname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Address { get; set; }
+    }
+}
