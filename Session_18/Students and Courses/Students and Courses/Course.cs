ad0e32ddf84539c6ef3bb43260537aae8e5d47f2
iakovosl <iakovoslampadakis@gmail.com>
2021-06-29 00:04:42 +0300

    Added classes

---
+++ Session_18/Students and Courses/Students and Courses/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students_and_Courses {
+    class Course {
+        public string Title { get; set; }
+        public string Category{ get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ Session_18/Students and Courses/Students and Courses/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students_and_Courses {
+    class Student {
+        public string Name { get; set; }
+        public string Sutname { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public string Address { get; set; }
+    }
+}
