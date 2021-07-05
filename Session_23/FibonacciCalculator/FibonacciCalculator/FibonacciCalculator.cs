26f89bd347e191129b585c3610a9c7521f746093
iakovosl <iakovoslampadakis@gmail.com>
2021-07-05 21:25:09 +0300

    Added implementation with specifications

--- Session_23/FibonacciCalculator/FibonacciCalculator/FibonacciCalculator.cs
+++ Session_23/FibonacciCalculator/FibonacciCalculator/FibonacciCalculator.cs
@@ -1,7 +1,23 @@
 ﻿using System;
 
-namespace FibonacciCalculator {
-    public class FibonacciCalculator {
+namespace FibonacciCalculator
+    {
+    public class FibonacciCalculator : IFibonacciCalculator {
+        public long FibonacciSeries(long n) {
 
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else if (n > 1 && n < 90)
+                return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+            else if (n < 0)
+                throw new ArgumentException("Invalid input..", nameof(n)); // It returns a simple string as a result. 
+            else
+                throw new ArgumentException("Fibonacci result number too big..", nameof(n));
+
+        }
     }
 }
+    
+
