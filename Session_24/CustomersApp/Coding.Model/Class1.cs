8ea43b7407b07ee21c08a6040b0d3da86366c0ec
iakovosl <iakovoslampadakis@gmail.com>
2021-07-07 21:58:16 +0300

    Added Customer Model

---
+++ Session_24/CustomersApp/Coding.Model/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Coding.Model {
+    public class Customer {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string AFM { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
---
+++ Session_24/CustomersApp/CustomersApp/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomersApp.Models {
+    public class Customer {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string AFM { get; set; }
+        public Boolean Active { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+       // public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
